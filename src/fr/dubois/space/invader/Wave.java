f9166ecf08513f13fa60065a0460efd44c333271
marine.cario <marine.cario@103F05.etu.iut-lens.univ-artois.fr>
2012-12-10 12:01:49 +0100

    fichier wave

---
+++ src/fr/dubois/space/invader/Wave.java
@@ -0,0 +1,34 @@
+package fr.dubois.space.invader;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+
+public class Wave {
+
+Wave Wave ;
+	
+	public Wave(Context context) {
+		super(context);
+		init();
+	}
+
+	public Wave(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init();
+	}
+
+	public Wave(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+
+
+}
+
+// insérer bitmap avec un paramètre constructeur ainsi que liste d'alien contenant un alien
+
+//Charles, il faudra renommer l'alien en wave, voir page 33
+
