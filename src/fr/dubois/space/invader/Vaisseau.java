7c78d6342f345f2d7288932f2319d350a1a6f1a5
marine.cario <marine.cario@103F05.etu.iut-lens.univ-artois.fr>
2013-01-09 15:19:47 +0100

    nouvelle modification

--- src/fr/dubois/space/invader/Vaisseau.java
+++ src/fr/dubois/space/invader/Vaisseau.java
@@ -3,16 +3,17 @@
 package fr.dubois.space.invader;
 
 import android.graphics.Bitmap;
-new Vaisseau 
-public abstract class Vaisseau1 extends Sprite {
+ 
+public abstract class Vaisseau extends Sprite {
+	Vaisseau Vaisseau ;
 
-	public Vaisseau1(Bitmap bitmap, float x, float y) {
+	public Vaisseau(Bitmap bitmap, float x, float y) {
 		super(bitmap, x, y);
 		// TODO Auto-generated constructor stub
 		
 	}
-	public  Vaisseau(Bitmap bitmap, float x, float y) {
+	public  void Vaisseau(Bitmap bitmap, float x, float y) {
 		super(bitmap, x, y);
 }
 
-	
+}
